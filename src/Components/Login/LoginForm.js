940d95f7e501c6246ac2f3cbeae94f11f9b42add
brito101 <contato@rodrigobrito.dev.br>
2021-11-14 11:35:18 -0300

    UseForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,40 +1,43 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import useForm from "../../Hooks/useForm"
 import Button from "../Forms/Button"
 import Input from "../Forms/Input"
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("")
-  const [password, setPassword] = React.useState("")
+  const username = useForm()
+  const password = useForm()
 
   function handleSubmit(e) {
     e.preventDefault()
-    fetch(
-      "https://www.rodrigobrito.dev.br/projetos/api-dogs/json/jwt-auth/v1/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      }
-    )
-      .then((res) => {
-        console.log(res)
-        return res.json()
-      })
-      .then((json) => {
-        console.log(json)
-      })
+    if (username.validate() && password.validate()) {
+      fetch(
+        "https://www.rodrigobrito.dev.br/projetos/api-dogs/json/jwt-auth/v1/token",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      )
+        .then((res) => {
+          console.log(res)
+          return res.json()
+        })
+        .then((json) => {
+          console.log(json)
+        })
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action='' onSubmit={handleSubmit}>
-        <Input label='Usuário' type='text' name='username' />
-        <Input label='Senha' type='password' name='password' />
-        <Button>Entrar</Button> 
+        <Input label='Usuário' type='text' name='username' {...username} />
+        <Input label='Senha' type='password' name='password' {...password} />
+        <Button>Entrar</Button>
       </form>
       <Link to='/login/criar'>Cadastro</Link>
     </section>
