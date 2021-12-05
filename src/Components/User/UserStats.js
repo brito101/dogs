057082323f79e4cc9562826c7ebebf9ef80e791b
brito101 <rodrigobrito101@hotmail.com>
2021-12-05 08:54:38 -0300

    Add Victory Chart

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,37 @@
 import React from "react"
 import Head from "../Helper/Head"
+import useFetch from "../../Hooks/useFetch"
+import { STATS_GET } from "../../Api"
+import Loading from "../Helper/Loading"
+import Error from "../Helper/Error"
+import UserStatsGraphs from "./UserStatsGraphs"
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-    </div>
-  )
+  const { data, error, loading, request } = useFetch()
+
+  React.useEffect(() => {
+    // eslint-disable-next-line no-unused-vars
+    let isMounted = true
+    async function getData() {
+      const { url, options } = STATS_GET()
+      await request(url, options)
+    }
+    getData()
+    return () => {
+      isMounted = false
+    }
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data)
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data} />
+      </div>
+    )
+  else return null
 }
 
 export default UserStats
